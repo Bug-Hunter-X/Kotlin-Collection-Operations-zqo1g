d52ce53a6e79bee4c8451883ff437a074d2e3bb3
BugHunter-X <askzenai@gmail.com>
2025-01-01 22:32:05 +0100

    Add CollectionOperations.kt file

---
+++ CollectionOperations.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val doubledList = list.map { it * 2 }
+    println(doubledList) // Output: [2, 4, 6, 8, 10]
+
+    val filteredList = list.filter { it % 2 == 0 }
+    println(filteredList) // Output: [2, 4]
+
+    val sum = list.fold(0) { acc, i -> acc + i }
+    println(sum) // Output: 15
+
+    val average = list.average()
+    println(average) // Output: 3.0
+}
